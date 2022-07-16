2c1fcfd161f3038d45cacdc8d92cec3250e8c3d2
AngelicaTheran <atheran082@gmail.com.com>
2022-07-16 14:11:07 -0500

    add light and dark mode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+let typed = new Typed(".typing", {
+    strings:["", "Web Developer", "Web Designer", "Youtuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
